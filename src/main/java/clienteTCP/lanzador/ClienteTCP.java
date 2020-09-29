1c3df13242a943539688ed12db39ee356df8ecee
VictorMardones <v.mardones04@ufromail.cl>
2020-09-29 01:40:09 -0300

    Empezar con las clases

---
+++ src/main/java/clienteTCP/lanzador/ClienteTCP.java
@@ -0,0 +1,12 @@
+package clienteTCP.lanzador;
+
+import java.net.Socket;
+
+public class ClienteTCP {
+
+    //// Métodos
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/main/java/servidorTCP/lanzador/ServidorTCP.java
@@ -0,0 +1,12 @@
+package servidorTCP.lanzador;
+
+import java.net.Socket;
+
+public class ServidorTCP {
+
+    //// Métodos
+    public static void main(String[] args){
+
+    }
+
+}
