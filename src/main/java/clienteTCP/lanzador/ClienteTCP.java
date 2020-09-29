303c471ac1588444d49ca89f2f474197dfc93434
VictorMardones <v.mardones04@ufromail.cl>
2020-09-29 01:51:58 -0300

    Empezar a configurar los Socket

--- src/main/java/clienteTCP/lanzador/ClienteTCP.java
+++ src/main/java/clienteTCP/lanzador/ClienteTCP.java
@@ -4,8 +4,22 @@ import java.net.Socket;
 
 public class ClienteTCP {
 
+    //// Atributos
+    /**
+     * Socket del cliente
+     */
+    private static Socket cliente;
+
     //// Métodos
     public static void main(String[] args){
+
+        System.out.println("Bienvenido al cliente TCP");
+
+        try{
+            cliente = new Socket("localhost", 80);
+        } catch(Exception e){
+            System.out.println("Error de conexión");
+        }
 
     }
 
--- src/main/java/servidorTCP/lanzador/ServidorTCP.java
+++ src/main/java/servidorTCP/lanzador/ServidorTCP.java
@@ -1,11 +1,31 @@
 package servidorTCP.lanzador;
 
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorTCP {
 
+    //// Atributos
+    /**
+     * Socket del servidor
+     */
+    private static ServerSocket servidor;
+    /**
+     * Socket para aceptar la conexión al servidor
+     */
+    private static Socket aceptarConexion;
+
     //// Métodos
     public static void main(String[] args){
+
+        System.out.println("Bienvenido al servidor TCP");
+
+        try{
+            servidor = new ServerSocket(80);
+            aceptarConexion = servidor.accept();
+        }catch(Exception e){
+            System.out.println("Error de conexión");
+        }
 
     }
 
